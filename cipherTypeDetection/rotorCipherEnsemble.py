a616ed8793e2727a67479066757f042996c32d99
MaikBastian <2962185+MaikBastian@users.noreply.github.com>
2023-11-20 14:18:35 +0100

    Improve `rotorCipherEnsemble`

--- cipherTypeDetection/rotorCipherEnsemble.py
+++ cipherTypeDetection/rotorCipherEnsemble.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
 from cipherTypeDetection.textLine2CipherStatisticsDataset import calculate_histogram
 from cipherTypeDetection.textLine2CipherStatisticsDataset import calculate_digrams
 from cipherTypeDetection.textLine2CipherStatisticsDataset import calculate_cipher_sequence
@@ -8,8 +11,9 @@ rotor_classes = ["Enigma", "M209", "Purple", "Sigaba", "Typex"]
 number_of_rotor_classes = 5
 
 class RotorCipherEnsemble:
-    def __init__(self, models):
+    def __init__(self, models, scaler):
         self.models = models
+        self.scaler = scaler
         
     def predict_single_line(self, ciphertext_line):
         features = [calculate_histogram(ciphertext_line) +
@@ -17,7 +21,12 @@ class RotorCipherEnsemble:
                     calculate_cipher_sequence(ciphertext_line)]
         prediction = [0] * number_of_rotor_classes
         for model in self.models:
-            model_prediction = model.predict_proba(features)
+            # TODO: Not only SVCs need a scaler
+            if isinstance(model, (SVC)):
+                features_scaled = self.scaler.transform(features)
+                model_prediction = model.predict_proba(features_scaled)
+            else:
+                model_prediction = model.predict_proba(features)
             prediction = np.add(prediction, model_prediction[0])
             
         # divide the combined predictions by the number of models
